adf872d4f020db618780b20a75c46764383d542a
GCC <germancardenasc@gmail.com>
2026-02-27 16:28:16 -0300

    added the deploy file por github actions

--- ui/api_client.py
+++ ui/api_client.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-BASE_URL = os.getenv("API_URL", "https://compliance-copilot-6wrw.onrender.com/api")
+BASE_URL = os.getenv("API_URL", "http://backend:8000/api")
 
 class ComplianceAPIClient:
     def __init__(self):
